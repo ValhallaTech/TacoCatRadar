9b39cd78c5275609088e6800eef9b36643940275
Fred <valhallatechnc@gmail.com>
2020-10-07 17:12:28 -0400

    Updated local directory

---
+++ Scripts/loop2loop.js
@@ -0,0 +1,24 @@
+﻿//For Loop work space
+document.getElementById("btnFlip").addEventListener("click", function() {
+    let inputWord = document.getElementById("inputWord").value;
+    let outputWord = document.getElementById("outputWord").value;
+    let lowerInput = inputWord.toLowerCase();
+    
+    for (let loop = inputWord.length - 1; loop >= 0; loop--) {
+        outputWord += lowerInput.charAt(loop);
+    }
+    document.getElementById("outputWord").innerHTML = outputWord;
+});
+
+//function reverseWord() {
+//    let inputWord = document.getElementById("input").value;
+//    //let reverseWord = "";
+//    let lowerInput = inputWord.toLowerCase();
+//    for (let loop = inputWord.length - 1; loop >= 0; loop--) {
+//        reverseWord += lowerInput.charAt(loop);
+//    }
+//}
+
+//let otherReverse = lowerInput.split("").reverse().join("");
+    //let otherReverse = lowerInput.split
+//let otherReverse = ""
