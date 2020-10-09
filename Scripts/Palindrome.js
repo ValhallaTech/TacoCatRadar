ac2068e1101f8bdb7202b066103c62a6c4e7cd15
Fred <valhallatechnc@gmail.com>
2020-10-08 22:23:03 -0400

    Updated palindrom.js
    Wired up for a split.reverse.join in place of a for loop
    
    Added sweetalerts.min.js 2.1.2

--- Scripts/Palindrome.js
+++ Scripts/Palindrome.js
@@ -1,24 +1,26 @@
-﻿//For Loop work space
+﻿
+function magicReverse(lowerInput) {
+    let outputWord = lowerInput.split("").reverse().join("");
+    document.getElementById("outputBox").innerHTML = outputWord;
+    return outputWord;
+}
+
 document.getElementById("btnFlip").addEventListener("click", function() {
-    let inputWord = document.getElementById("inputWord").value;
-    let outputWord = document.getElementById("outputWord").value;
-    let lowerInput = inputWord.toLowerCase();
-    
-    for (let loop = inputWord.length - 1; loop >= 0; loop--) {
-        outputWord += lowerInput.charAt(loop);
-    }
-    document.getElementById("outputWord").innerHTML = "Error" ?? outputWord;
+    let inputWord = document.getElementById("inputBox").value;
+    let outputWord = inputWord.toLowerCase();
+    let reversedWord = magicReverse(outputWord);
 });
 
-//function reverseWord() {
-//    let inputWord = document.getElementById("input").value;
-//    //let reverseWord = "";
-//    let lowerInput = inputWord.toLowerCase();
-//    for (let loop = inputWord.length - 1; loop >= 0; loop--) {
-//        reverseWord += lowerInput.charAt(loop);
-//    }
-//}
+if (outputWord === reversedWord) {
+    document.getElementById()
+    
+}
 
-//let otherReverse = lowerInput.split("").reverse().join("");
-    //let otherReverse = lowerInput.split
-//let otherReverse = ""
+//For Loop work space
+//    for (let loop = inputWord.length - 1; loop >= 0; loop--) {
+//        outputWord += lowerInput.charAt(loop);
+//    }
+//    document.getElementById("outputWord").innerText = outputWord;
+//});
+
+//Identify valdid palindrome
