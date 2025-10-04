b0b5c37f7607951d2e98809b3f071061ee0fb0bb
Fred <valhallatechnc@gmail.com>
2025-10-03 20:46:01 -0400

    Upgrade to Bootstrap 5 and update dependencies
    Migrated from Bootstrap 4 to Bootstrap 5, replaced jQuery and confetti-js with canvas-confetti, and switched from sweetalert to sweetalert2. Updated HTML files for Bootstrap 5 compatibility and refactored Palindrome.js to use new libraries. Updated package.json and yarn.lock to reflect new dependencies.

--- Scripts/Palindrome.js
+++ Scripts/Palindrome.js
@@ -1,9 +1,8 @@
 ﻿// Import dependencies
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import $ from 'jquery';
-import confetti from 'confetti-js';
-import swal from 'sweetalert';
+import confetti from 'canvas-confetti';
+import Swal from 'sweetalert2';
 
 // Import custom CSS
 import '../content/main.css';
@@ -24,43 +23,24 @@ document.getElementById("btnFlip").addEventListener("click",
     function () {
         let inputStr = document.getElementById("inputBox").value;
         let reversedStr = fncReverse02();
-        let fncSuccess01 = swal({
-            title: "You Haz Palindrome!",
-            icon: "success",
-        });
+        
         if (inputStr === reversedStr) {
-            confetti.start(1000);
-            fncSuccess01;
+            // Trigger confetti celebration
+            confetti({
+                particleCount: 100,
+                spread: 70,
+                origin: { y: 0.6 }
+            });
+            
+            Swal.fire({
+                title: "You Haz Palindrome!",
+                icon: "success",
+            });
         }
         else {
-            swal({
+            Swal.fire({
                 title: "Nope. Palindrome Failz...",
                 icon: "error",
             });
         }
     });
-
-
-
-
-
-//function reverseMe() {
-//}
-
-//if (outputWord === reversedWord) {
-//    document.getElementById()
-
-//}
-//For Loop work space
-//    for (let loop = inputWord.length - 1; loop >= 0; loop--) {
-//        outputWord += lowerInput.charAt(loop);
-//    }
-//    document.getElementById("outputWord").innerText = outputWord;
-//});
-
-//Identify valid palindrome
-//function funReverse01(lowerInput) {
-//    let outputWord = lowerInput.split("").reverse().join("");
-//    document.getElementById("outputBox").innerHTML = outputWord;
-//    return outputWord;
-//}
